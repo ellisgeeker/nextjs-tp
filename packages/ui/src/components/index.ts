76f898bfe45751c8eea7dcf7455fc4fbc25c6b03
ellis <codecoolgeeker@gmail.com>
2026-01-06 13:15:05 +0800

    feat(ui): add base components, hooks and styling setup

---
+++ packages/ui/src/components/index.ts
@@ -0,0 +1,3 @@
+export { Button, type ButtonProps } from "./button";
+export { Label, type LabelProps } from "./label";
+export { Input, type InputProps } from "./input";
---
+++ packages/ui/src/primitives/index.ts
@@ -0,0 +1,76 @@
+// Dialog
+export {
+  Dialog,
+  DialogTrigger,
+  DialogPortal,
+  DialogClose,
+  DialogOverlay,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  ComposedDialog,
+  type DialogOverlayProps,
+  type DialogContentProps,
+  type DialogTitleProps,
+  type DialogDescriptionProps,
+  type ComposedDialogProps,
+} from "./dialog";
+
+// Tooltip
+export {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipPortal,
+  TooltipArrow,
+  TooltipContent,
+  SimpleTooltip,
+  type TooltipContentProps,
+  type SimpleTooltipProps,
+} from "./tooltip";
+
+// DropdownMenu
+export {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuPortal,
+  DropdownMenuGroup,
+  DropdownMenuSub,
+  DropdownMenuRadioGroup,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+  DropdownMenuShortcut,
+  type DropdownMenuContentProps,
+  type DropdownMenuItemProps,
+  type DropdownMenuCheckboxItemProps,
+  type DropdownMenuRadioItemProps,
+  type DropdownMenuLabelProps,
+  type DropdownMenuSeparatorProps,
+  type DropdownMenuSubTriggerProps,
+  type DropdownMenuSubContentProps,
+} from "./dropdown-menu";
+
+// Select
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectPortal,
+  SelectIcon,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  type SelectTriggerProps,
+  type SelectContentProps,
+  type SelectLabelProps,
+  type SelectItemProps,
+  type SelectSeparatorProps,
+} from "./select";
